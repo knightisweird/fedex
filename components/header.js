a58fba12cb667fc43b56e875faa83ce82495bcbe
knightisweird <nairateam13@gmail.com>
2024-10-11 07:07:43 -0700

    change search icon tag

--- components/header.js
+++ components/header.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
 import logo from '@/assets/logo.png'
-import search from '@/assets/search.png'
+import searchIcon from '@/assets/search.png'
 
 const Header = () => {
   return (
     <header className="w-full bg-[#4d148c] p-6">
       <div className="max-w-[1200px] mx-auto flex items-center justify-between">
         <Image src={logo} width={80} height={60} />
-        <Image src={search} width={30} height={30} className="cursor-pointer" />
+        <Image src={searchIcon} width={30} height={30} className="cursor-pointer" />
       </div>
     </header>
   )
