7e6fcb9277a4b5f44796cb96cba1224bc9c1cee3
knightisweird <nairateam13@gmail.com>
2024-10-11 07:11:22 -0700

    change search icon path

--- components/header.js
+++ components/header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import logo from '@/assets/logo.png'
-import searchIcon from '@/assets/search.png'
+import searchIcon from '@/assets/searchIcon.png'
 
 const Header = () => {
   return (
